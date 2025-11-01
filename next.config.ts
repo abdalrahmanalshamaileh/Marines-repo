58401eedfda9e92e51301811875c49048ac0bf31
raed-sh <raedsh.work@gmail.com>
2025-11-01 16:27:16 +0300

    chore: change repo name

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: isProd ? "/WujoodCare" : "", // TODO: this should follow the domain
-  assetPrefix: isProd ? "/WujoodCare" : "",
+  basePath: isProd ? "/Marines-repo" : "", // TODO: this should follow the domain
+  assetPrefix: isProd ? "/Marines-repo" : "",
   images: {
     unoptimized: true,
   },
