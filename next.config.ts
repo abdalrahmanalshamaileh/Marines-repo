ea0e1996d2e54186459c5785871e3b8e84680a11
Abdalrahman alshamaileh <81919326+abdalrahmanalshamaileh@users.noreply.github.com>
2026-05-31 23:37:36 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
+  basePath: isProd ? "/Marines-repo" : "", // TODO: this should follow the domain
+  assetPrefix: isProd ? "/Marines-repo" : "",
   images: {
     unoptimized: true,
   },
